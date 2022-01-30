e5152bd33a8dce2808ace8ecd66e601c3962cc06
M4RMU <97147718+B4BY-DG@users.noreply.github.com>
2022-01-30 20:39:54 +0630

    Update AWD.py

--- AWD.py
+++ AWD.py
@@ -81,12 +81,12 @@ os.system('clear')
 loop = 'true'
 while loop == 'true':
     os.system (" toilet -F gay -f slant AYARWADY")
-    username = raw_input('\x1b[1;97m\xe2\x9e\xa3 USERNAME TOOL=>>: \33[1;96m')
+    username = raw_input('\x1b[1;97m👹 USERNAME TOOL=>>: \33[1;96m')
     if username == B4BY:
-        password = raw_input('\x1b[1;97m\xe2\x9e\xa3 PASSWORD TOOL =>>: \033[1;96m')
+        password = raw_input('\x1b[1;97m👹 PASSWORD TOOL =>>: \033[1;96m')
         if password == DRAGON:
             print '\x1b[1;92m[\xe2\x9c\x93] HELLO GONG GYI!' + username
-            jalan ("\033[1;96m DO YOU KNOW M4RMU'
+            jalan ("\033[1;96m DO YOU KNOW M4RMU")
             jalan ('\033[1;97m NOW YOU READY TO USE THIS TOOL')
             time.sleep(2)
             loop = 'false'
@@ -108,7 +108,7 @@ id=[]
 def main():
     os.system('clear')
     print logo
-    print ' \t [\x1b[1;97m\x1b[1;41m   If Any Problem So Contact Me   \x1b[0m\x1b[1;93m]'.center(50)
+    print ' \t [\x1b[1;97m\x1b[1;41m   This tool is equation proposed only ..   \x1b[0m\x1b[1;93m]'.center(50)
     print 54 * '\x1b[1;93m_'
     print ''
     print 47 * '\x1b[1;93m\xe2\x96\xac'
